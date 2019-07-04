2eb2a40c422a155d4aa74bfb57f32fe1eeb8be47
Eliza Munroe <era3munroe@gmail.com>
2019-07-04 11:37:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,7 @@ def monopoly_with_third_tier
   monopoly = {:railroads => {
                    :pieces => 4, 
                    :names => {},
-                   :rent_in_dollars => [{}]
-     } 
+                   :rent_in_dollars => {} } 
     }
 end
 
